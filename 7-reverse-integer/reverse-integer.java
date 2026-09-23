08f44801618545503b037c8821f9a2e18b6b5e9a
piyushraj5919 <rajpiyush122004@gmail.com>
2026-09-23 14:08:19 +0530

    Time: 1 ms (99.96%) | Memory: 42.7 MB (26.89%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+
+        long reverse = 0;
+
+        while(x != 0){
+            int rem = x % 10;
+            reverse = reverse * 10 + rem;
+            x = x / 10;
+        }
+        if (reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int) reverse;
+        
+    }
+}
